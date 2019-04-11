1597a38614e72ed49cc6cd874bed1d6e1b870510
QianXi <qianxiwangluokejiyouxiangongsi@qianxiwangluokejiyouxiangongsideiMac-2.local>
2019-04-11 17:13:42 +0800

    Initial JKSelectTextField

---
+++ Sources/JKInputTableView.h
@@ -0,0 +1,30 @@
+//
+//  JKInputTableView.h
+//  JKInputTableView
+//
+//  Created by iMac on 2017/1/10.
+//  Copyright © 2017年 Jack Liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class JKInputTableView;
+
+@protocol JKInputTableViewDelegate <NSObject>
+
+-(void)inputTableView:(nullable JKInputTableView *)inputTableView didSelectRowAtIndexPath:(nullable NSIndexPath *)indexPath object:(nullable id)object;
+
+-(void)cancelSelect;
+
+@end
+
+
+@interface JKInputTableView : UITableView<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong, nullable) NSMutableArray *Options;
+
+@property (nonatomic, weak, nullable) id <JKInputTableViewDelegate> JKIPTDelegate;
+
+@property (nonatomic, strong, nullable) UIView *responderView;
+
+@end
