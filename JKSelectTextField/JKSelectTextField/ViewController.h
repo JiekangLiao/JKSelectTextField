d5590fbd1f89de1af26c10933162eb6f5a8b2bb2
QianXi <qianxiwangluokejiyouxiangongsi@qianxiwangluokejiyouxiangongsideiMac-2.local>
2019-04-12 11:11:22 +0800

    create demo

---
+++ JKSelectTextField/JKSelectTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKSelectTextField
+//
+//  Created by QianXi on 2019/4/12.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JKSelectTextField/JKSelectTextField/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JKSelectTextField
+//
+//  Created by QianXi on 2019/4/12.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Sources/JKSelectTextField.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    JKSelectTextField *selectTF = [[JKSelectTextField alloc]initWithFrame:CGRectMake(15, 200, UIScreen.mainScreen.bounds.size.width, 50)];
+    selectTF.backgroundColor = UIColor.groupTableViewBackgroundColor;
+    selectTF.options = [[NSMutableArray alloc]initWithObjects:@"选项1", @"选项2", @"选项3", nil];
+    [self.view addSubview:selectTF];
+}
+
+
+@end
